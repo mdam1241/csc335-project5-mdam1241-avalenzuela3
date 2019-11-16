2030ff3917cec996a59306027d5f92e1f9c4ae78
Aaron <Aaron@DESKTOP-QBGC3EA>
2019-11-16 15:59:34 -0700

    Set up the GUI

--- src/Connect4View.java
+++ src/Connect4View.java
@@ -2,7 +2,17 @@ import java.util.Observable;
 import java.util.Observer;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 /**
  * @author Michael Dam, Aaron Valenzuela
@@ -18,9 +28,37 @@ public class Connect4View extends Application implements Observer {
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Connect 4");
+				
+		GridPane grid = new GridPane();
 		
+		int row = 0;
+		int col = 0;
+		for (int i = 0; i < 42; i++) {
+			if (i % 7 == 0) {
+				row ++;
+				col = 0;
+			}
+			
+			Circle circle = new Circle(20, Color.WHITE);
+			VBox box = new VBox(circle);
+			
+			box.setCenterShape(true);
+
+			grid.add(box, col, row);
+			col++;
 	}
+		
+	grid.setVgap(8);
+	grid.setHgap(8);
+		
+	grid.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+	grid.setPadding(new Insets(10));
 	
+	Scene scene = new Scene(grid, 350, 310); 
+	stage.setScene(scene);
+	// show the running app:
+	stage.show();
+}
 }
