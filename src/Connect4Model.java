ec5588c990ea910e9b88cbc34abda13070b53809
Michael Dam <mdam1241@email.arizona.edu>
2019-11-16 15:12:01 -0700

    Model - clearBoard()

--- src/Connect4Model.java
+++ src/Connect4Model.java
@@ -6,11 +6,20 @@ import java.util.Observable;
  */
 
 public class Connect4Model extends Observable {
-	public int row; // # of rows in board
-	public int col; // # of columns in board
+	public int row; 		// # of rows in board
+	public int col; 		// # of columns in board
+	private int[][] board;	// 0 means empty, 1 means yellow, 2 means red
 	
 	public Connect4Model() {
 		row = 6;
 		col = 7;
+		board = new int[row][col];
+	}
+	
+	/**
+	 * Clears the old board, for starting new games.
+	 */
+	public void clearBoard() {
+		board = new int[row][col];
 	}
 }
