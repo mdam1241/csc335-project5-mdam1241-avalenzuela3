fb73dcbd5ed072a740744efed98d353e2893da69
Michael Dam <mdam1241@email.arizona.edu>
2019-11-16 17:46:07 -0700

    Created test cases that covers every victory case

--- src/Connect4Tests.java
+++ src/Connect4Tests.java
@@ -9,11 +9,19 @@ import org.junit.jupiter.api.Test;
 
 public class Connect4Tests {
 	
-
 	@Test
 	void testBoardVictoryDown() {
 		Connect4Model board = new Connect4Model();
-		assertFalse(board.checkVictory() == 1);
+		for (int i = 0; i < 4; i++) {
+			board.getBoard()[i][0] = 1;
+		}
+		board.printDebug();
+		assertTrue(board.checkVictory() == 1);
+	}
+
+	@Test
+	void testBoardVictoryRight() {
+		Connect4Model board = new Connect4Model();
 		for (int i = 0; i < 4; i++) {
 			board.getBoard()[0][i] = 1;
 		}
@@ -22,22 +30,36 @@ public class Connect4Tests {
 	}
 	
 	@Test
+	void testBoardVictoryDownRight() {
+		Connect4Model board = new Connect4Model();
+		for (int i = 0; i < 4; i++) {
+			board.getBoard()[i][i] = 1;
+		}
+		board.printDebug();
+		assertTrue(board.checkVictory() == 1);
+	}
+	
+	@Test
+	void testBoardVictoryUpRight() {
+		Connect4Model board = new Connect4Model();
+		for (int i = 0; i < 4; i++) {
+			board.getBoard()[5 - i][i] = 1;
+		}
+		board.printDebug();
+		assertTrue(board.checkVictory() == 1);
+	}
+	
+	@Test
 	void testDrop() {
 		Connect4Model board = new Connect4Model();
-		board.printDebug();
 		board.dropToken(1, 0);
-		board.printDebug();
 		board.dropToken(1, 0);
-		board.printDebug();
 		board.dropToken(1,1);
-		board.printDebug();
 		board.dropToken(2, 6);
 		board.printDebug();
 		assertFalse(board.checkVictory() > 0);
 		board.dropToken(1, 0);
-		board.printDebug();
 		board.dropToken(2, 0);
-		board.printDebug();
 		board.dropToken(1, 0);
 		board.printDebug();
 		assertFalse(board.checkVictory() > 0);
